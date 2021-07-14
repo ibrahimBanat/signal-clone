89022bd3b9899c091f720d70e97e6ab246028b0c
IbrahimBanat <ibrahim.banat.97@gmail.com>
2021-07-14 17:38:57 +0300

    finish the sign in page and add some style and element

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Button, Input, Image } from 'react-native-elements';
+import { KeyboardAvoidingView } from 'react-native';
 const loginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = () => {};
   return (
-    <View>
+    <KeyboardAvoidingView behavior='padding' styles={styles.container}>
       <StatusBar style='light' />
       <Image
         source={{
@@ -13,11 +18,44 @@ const loginScreen = () => {
         }}
         style={{ width: 200, height: 200 }}
       />
-      <View styles={styles.inputContainer}></View>
-    </View>
+      <View styles={styles.inputContainer}>
+        <Input
+          placeholder='Email'
+          autoFocus
+          type='Email'
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <Input
+          placeholder='Password'
+          secureTextEntry
+          type='password'
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
+      </View>
+      <Button title='Login' containerStyle={styles.button} onPress={signIn} />
+      <Button title='Register' containerStyle={styles.button} type='outline' />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
   );
 };
 
 export default loginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  inputContainer: {
+    width: 300,
+  },
+});
